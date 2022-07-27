0c0c857c812928bccfb09e4803bd24ead1fc1c51
skrimnir <105659267+skrimnir@users.noreply.github.com>
2022-07-27 22:21:36 +0300

    add b record

--- keyboards.py
+++ keyboards.py
@@ -2,6 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 #Клавиатура клиента
 b_lesson = KeyboardButton('/Список_занятий')
+b_record = KeyboardButton('/Запсаться')
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -15,6 +16,3 @@ b_cancel = KeyboardButton("Отмена_загрузки")
 kb_admin = ReplyKeyboardMarkup(resize_keyboard=True)
 
 kb_admin.add(b_load).add(b_delete).add(b_cancel)
-
-
-
